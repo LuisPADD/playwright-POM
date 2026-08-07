942ecb16032d4bd938822f89f180da146420e4c3
LuisPADD <maryofgod1706@gmail.com>
2026-08-07 18:05:16 -0300

    create Page Objects

--- tests/test_001_login_successful.py
+++ tests/test_001_login_successful.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import Page, expect
 
 
-def test_001_login_successful(page: Page) -> None:
+def test_001_login_successful(page, login_page) -> None:
     page.goto("https://leogcarvalho.github.io/simulabank/login.html")   
-    page.get_by_role("textbox", name="Usuário:").fill("user1")   
-    page.get_by_role("textbox", name="Senha:").fill("pass1")
-    page.get_by_role("button", name="Entrar").click()
-    expect(page.get_by_role("heading", name="Bem-vindo ao SimulaBank!")).to_be_visible()
+    login_page.login("user1","pass1")
+    login_page.assert_login_successful()
+
+    
