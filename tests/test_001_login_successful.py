8fa7a3b28041f754dc4cf655f527e23f41218dab
LuisPADD <maryofgod1706@gmail.com>
2026-08-09 04:51:18 -0300

    Add page object classes and update tests for login and Pix functionality

--- tests/test_001_login_successful.py
+++ tests/test_001_login_successful.py
@@ -2,7 +2,6 @@ from playwright.sync_api import Page, expect
 
 
 def test_001_login_successful(page, login_page) -> None:
-    page.goto("https://leogcarvalho.github.io/simulabank/login.html")   
     login_page.login("user1","pass1")
     login_page.assert_login_successful()
 
